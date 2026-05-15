98915e44d3a8d2b8f624130573ac9695d459eeab
henriquefirbidagirotto-cmd <henriquefirbidagirotto@gmail.com>
2026-05-14 21:34:19 -0300

    Update app_view.py

--- app/views/app_view.py
+++ app/views/app_view.py
@@ -47,7 +47,7 @@ if st.session_state.token:
     for l in livros:
         with st.container(border=True):
             col_info, col_del = st.columns([4, 1])
-            col_info.write(f"**{l['titulo']}** | {l['autor']}")
+            col_info.write(f"**{l['Carro']}** | {l['Ano']}")
             if col_del.button("Excluir", key=f"del_{l['id']}"):
                 requests.delete(f"{API_URL}/livros/{l['id']}")
                 st.rerun()
